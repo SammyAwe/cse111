56b7b231aa751e1284c5c888a6ab194c0420fff7
SammyAwe <awesamuelopeoluwa@gmail.com>
2024-01-10 09:41:11 -0700

    created a short program to calculate tire volume

---
+++ tire_volume.py
@@ -0,0 +1,13 @@
+import math
+
+width = float(input("Enter the width of the tire in millimeters: "))
+aspect_ratio = float(input("Enter the aspect ratio of the tire: "))
+diameter = float(input("Enter the diameter of the wheel in inches: "))
+print()
+volumepart1 = (math.pi * (width ** 2) * aspect_ratio)
+volumepart2 = width * aspect_ratio + 2540 * diameter
+finalvolume = volumepart1 * volumepart2 / 10000000000
+
+
+
+print(f"The volume of space inside the tire is: {finalvolume:.2f} liters")
