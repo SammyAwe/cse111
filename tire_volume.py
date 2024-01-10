fa81c90ef582b3befc1744dcfdbd7f24975f7a3a
SammyAwe <awesamuelopeoluwa@gmail.com>
2024-01-10 09:47:56 -0700

    first py commit

--- tire_volume.py
+++ tire_volume.py
@@ -4,6 +4,7 @@ width = float(input("Enter the width of the tire in millimeters: "))
 aspect_ratio = float(input("Enter the aspect ratio of the tire: "))
 diameter = float(input("Enter the diameter of the wheel in inches: "))
 print()
+#broke down my calculation in order to make it simpler
 volumepart1 = (math.pi * (width ** 2) * aspect_ratio)
 volumepart2 = width * aspect_ratio + 2540 * diameter
 finalvolume = volumepart1 * volumepart2 / 10000000000
